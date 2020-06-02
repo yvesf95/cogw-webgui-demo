7c1635431b0563c364ce04a35352011601ef4dd6
Yves Francisco <yvesfrancisco95@gmail.com>
2020-06-02 15:10:59 +0800

    added vue2-google-maps

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,43 @@
+<template>
+  <gmap-map :center="center" :zoom="17" style="width:100%;  height: 600px;">
+    <!-- <gmap-marker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      @click="center = m.position"
+    ></gmap-marker> -->
+    <template v-for="(client, i) in clients">
+      <gmap-marker
+        v-if="client.active"
+        :key="i"
+        :position="client.currentPos"
+        :clickable="true"
+        :draggable="true"
+        @click="center = client"
+      ></gmap-marker>
+    </template>
+  </gmap-map>
+</template>
+
+<script>
+export default {
+  name: "GoogleMap",
+  props: ["clients"],
+  data() {
+    return {
+      center: {
+        lat: 14.554768,
+        lng: 121.044357
+      },
+      // markers: [
+      //   {
+      //     position: {
+      //       lat: 14.554768,
+      //       lng: 121.044357
+      //     }
+      //   }
+      // ]
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'; 
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    // Create .env on your root directory
+    // Add an environment variable named VUE_APP_GMAPS_KEY=<key>
+    key: process.env.VUE_APP_GMAPS_KEY,
+    libraries: "places" // necessary for places input
+  }
+});
 
 new Vue({
   vuetify,
